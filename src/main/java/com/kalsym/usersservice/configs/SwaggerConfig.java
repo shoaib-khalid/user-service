efead6e874f5475bb837fe2b717e88aa7f97c26f
sarosh <sarosh.tariq@kalsym.com>
2021-02-25 12:33:01 +0500

    added auth in swagger

--- src/main/java/com/kalsym/usersservice/configs/SwaggerConfig.java
+++ src/main/java/com/kalsym/usersservice/configs/SwaggerConfig.java
@@ -1,12 +1,21 @@
 package com.kalsym.usersservice.configs;
 
+import com.google.common.collect.Lists;
+import java.util.Date;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -18,21 +27,58 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
     @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                //.paths(PathSelectors.any())
-                .apis(RequestHandlerSelectors.basePackage("com.kalsym.usersservice"))
+    public Docket userServiceApi() {
+
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .apiInfo(ApiInfo.DEFAULT)
+                //.forCodeGeneration(true)
+                //.genericModelSubstitutes(ResponseEntity.class)
+                //.ignoredParameterTypes(Pageable.class)
+                //.ignoredParameterTypes(java.sql.Date.class)
+                //.directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
+                //.directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
+                //.directModelSubstitute(java.time.LocalDateTime.class, Date.class)
+                //.securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(new ApiKey("Bearer", AUTHORIZATION_HEADER, "header")))
+                .useDefaultResponseMessages(false);
+        docket = docket.select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
+        return docket;
+
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("users-service API Document")
+        return new ApiInfoBuilder()
+                .title("users-service")
                 .description("Used to manage users, roles, authorities for clients.")
                 .termsOfServiceUrl("not added yet")
                 .license("not added yet")
                 .licenseUrl("").version("1.0").build();
     }
+
+    public static final String DEFAULT_INCLUDE_PATTERN = "/.*";
+
+    public SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("Bearer", authorizationScopes));
+    }
+
 }

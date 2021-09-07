068f51170efbe2adb66e60dad4c4ee2d06a04681
sarosh <sarosh.tariq@kalsym.com>
2021-09-07 13:46:44 +0500

    added swagger 2

--- src/main/java/com/kalsym/userservice/configs/SwaggerConfig.java
+++ src/main/java/com/kalsym/userservice/configs/SwaggerConfig.java
@@ -1,47 +1,81 @@
 package com.kalsym.userservice.configs;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+
+import com.google.common.collect.Lists;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  *
- * @author Sarosh
+ * @author 7cu
  */
 @Configuration
-//@OpenAPIDefinition
+@EnableSwagger2
 public class SwaggerConfig {
 
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
     @Bean
-    public OpenAPI customOpenAPI() {
+    public Docket productServiceApi() {
 
-        Components comps = new Components().addSecuritySchemes("Auth",
-                new SecurityScheme()
-                        .name("Auth")
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("Bearer"));
-        return new OpenAPI()
-                .info(apiInfo())
-                .addSecurityItem(new SecurityRequirement().addList("Auth"))
-                .components(comps);
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .apiInfo(ApiInfo.DEFAULT)
+                //.forCodeGeneration(true)
+                //.genericModelSubstitutes(ResponseEntity.class)
+                //.ignoredParameterTypes(Pageable.class)
+                //.ignoredParameterTypes(java.sql.Date.class)
+                //.directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
+                //.directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
+                //.directModelSubstitute(java.time.LocalDateTime.class, Date.class)
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(new ApiKey("Bearer", AUTHORIZATION_HEADER, "header")))
+                .useDefaultResponseMessages(false);
+        docket = docket.select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+        return docket;
 
     }
 
-    private Info apiInfo() {
-        Contact contact = new Contact()
-                .email("support@kalsym.com")
-                .name("KALSYM")
-                .url("https://kalsym.com");
-        return new Info()
-                .version("0.0.1-SNAPSHOT")
-                .description("Users managment and auth.")
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
                 .title("user-service")
-                .contact(contact);
+                .description("Used to manage users, roles, authorities for clients.")
+                .termsOfServiceUrl("not added yet")
+                .license("not added yet")
+                .licenseUrl("").version("1.0").build();
     }
 
+    public static final String DEFAULT_INCLUDE_PATTERN = "/.*";
+
+    public SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("Bearer", authorizationScopes));
+    }
 }

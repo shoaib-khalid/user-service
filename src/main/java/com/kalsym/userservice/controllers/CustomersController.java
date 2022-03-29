e0ceb9836f2cde885a0260f4adcb418746113266
taufik <taufik@kalsym.com>
2022-03-29 18:41:48 +0800

    buf fix

--- src/main/java/com/kalsym/userservice/controllers/CustomersController.java
+++ src/main/java/com/kalsym/userservice/controllers/CustomersController.java
@@ -422,6 +422,7 @@ public class CustomersController {
                         "HSID="+user.getId()+"; Domain=.symplified.it; Path=/; Expires="+expiryTimestamp+"; HttpOnly");
         responseHeaders.add("Set-Cookie", 
                         "SSID="+user.getId()+"; Domain=symplified.it; Path=/; Expires="+expiryTimestamp+"; Secure; HttpOnly");
+        responseHeaders.add("access-control-expose-headers","Set-Cookie");
         
         Logger.application.info(Logger.pattern, UserServiceApplication.VERSION, logprefix, "Add custom httpHeaders in response");
         

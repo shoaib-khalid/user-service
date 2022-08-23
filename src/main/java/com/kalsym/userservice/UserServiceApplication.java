ea376191411355d3a6a6f7128662d39dcece0e73
taufik <taufik@kalsym.com>
2022-08-23 14:57:55 +0800

    bug fix

--- src/main/java/com/kalsym/userservice/UserServiceApplication.java
+++ src/main/java/com/kalsym/userservice/UserServiceApplication.java
@@ -8,9 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class UserServiceApplication {
 
     public static String VERSION;

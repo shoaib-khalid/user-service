645265980924b4e192a6ed14df1cd455bc7ef1e0
taufik <taufik@kalsym.com>
2022-08-23 14:09:22 +0800

    bug fix

--- src/main/java/com/kalsym/userservice/SendHeartbeatScheduler.java
+++ src/main/java/com/kalsym/userservice/SendHeartbeatScheduler.java
@@ -44,8 +44,9 @@ public class SendHeartbeatScheduler {
    
     @Scheduled(fixedDelay = 600000)
     public void sendHeartbeat() throws Exception {
+        String logprefix = "SendHeartbeatScheduler";                        
+        Logger.application.info(Logger.pattern, UserServiceApplication.VERSION, logprefix, "Starting sendHeartbeat(). Scheduler Enabled:"+isEnabled);
         if (isEnabled) {
-            String logprefix = "SendHeartbeatScheduler";            
             String[] suffixList = {"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"};
             
             for (int x=0;x<suffixList.length;x++) {
